00cf2b56b3b78b7b79cc115c4b93fa18085ebeba
zenoo97 <jaehun4086@naver.com>
2024-11-25 17:54:26 +0900

    [Update] 라우팅 기본

--- learn-next/pages/_app.jsx
+++ learn-next/pages/_app.jsx
@@ -4,6 +4,9 @@ import Link from 'next/link';
 export default function App({ Component, pageProps }) {
 	return (
 		<div>
+			<nav>
+				<Link href="/home">홈</Link> | <Link href="/login">로그인</Link>
+			</nav>
 			<Component {...pageProps} />
 		</div>
 	);
